f3e6640926bb7907439bf3d070fda09c8cbdd6d7
ajah0ng <annajahong@gmail.com>
2025-02-08 00:55:18 +1100

    fixed tailwind configuration

---
+++ src/Page.jsx
@@ -0,0 +1,14 @@
+
+
+function Page() {
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold bg-red-500 p-4">Hello, Tailwind!</h1>
+      
+
+    </div>
+  );
+}
+
+export default Page
