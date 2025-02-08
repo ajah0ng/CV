372f004157800320bc88ab7a9fe57dcd867d4acf
ajah0ng <annajahong@gmail.com>
2025-02-08 15:13:31 +1100

    added all tabs

---
+++ src/sections/Experience.jsx
@@ -0,0 +1,19 @@
+
+export default function Experience() {
+    return (
+    <div className="mt-10 p-4">
+        <p className="text-gray-600 mt-2">
+        Hi All, Welcome to my interactive CV created with Vite, React & Tailwind CSS! 
+        </p>
+        <p className="text-gray-600 mt-2">
+        My name is Anna and I am currently a 3rd year at UNSW, pursuing degrees in Science and Computer Science with a major in Bioinformatics. 
+        Throughout my coursework, I’ve had the opportunity to delve into how computational and analytical tools can drive advancements in healthcare and biotechnology.
+        Seeing this transformative impact of technology on larger issues, I was not only inspired to reflect upon issues that were present in my own life, but I also found myself gravitating towards learning about issues experienced by others.
+        </p>
+        <p className="text-gray-600 mt-2">
+        My journey as an intern at a startup, leading Design Thinking workshops at YEP!, and stepping into the world of pitching for the first time at the Peter Farrell Cup (newsflash: we didn’t make it through) has been an incredible learning experience. Through the ups & “downs” I’ve gained invaluable skills in technical development, communication and navigating the complexities of bringing innovation to market.
+        I’m passionate about my ideas and putting my foot down every stage of the process, and I’m always interested in learning and understanding people.
+        </p>
+    </div>
+    );
+}
---
+++ src/sections/Leadership.jsx
@@ -0,0 +1,10 @@
+export default function Leadership() {
+
+    return {
+
+        
+
+
+    }
+
+}
